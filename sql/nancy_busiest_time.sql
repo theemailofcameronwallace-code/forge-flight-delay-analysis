276865ff8382a0edc59a0c52bd9b3d2847b95789
Nancy Luna <nlunayb04@gmail.com>
2026-05-27 16:35:57 -0700

    update

--- sql/nancy_busiest_time.sql
+++ sql/nancy_busiest_time.sql
@@ -1,39 +1,43 @@
+Create DATABASE project2;
+USE archive;
+
 SELECT 
 	CASE DAY_OF_WEEK
-	WHEN 1 THEN ‘Sunday’
-	WHEN 2 THEN ‘Monday’
-WHEN 3 THEN ‘Tuesday’
-	WHEN 4 THEN ‘Wednesday’
-WHEN 5 THEN ‘Thursday’
-	WHEN 6 THEN ‘Friday’
-WHEN 7 THEN ‘Saturday’
-END AS day of week, COUNT(DAY_OF_WEEK) AS ‘Num of Flights’
+	WHEN 1 THEN 'Sunday'
+	WHEN 2 THEN 'Monday'
+	WHEN 3 THEN 'Tuesday'
+	WHEN 4 THEN 'Wednesday'
+	WHEN 5 THEN 'Thursday'
+	WHEN 6 THEN 'Friday'
+	WHEN 7 THEN 'Saturday'
+    END 
+    AS 'day of week', 
+    COUNT(DAY_OF_WEEK) AS 'TotalFlights'
 FROM flights
 GROUP BY DAY_OF_WEEK
-ORDER BY ‘Num of Flights’ DESC;
+ORDER BY 'TotalFlights' DESC;
 
 SELECT 
 	CASE MONTH
-	WHEN 1 THEN ‘January’
-	WHEN 2 THEN ‘February’
-WHEN 3 THEN ‘March’
-	WHEN 4 THEN ‘April’
-WHEN 5 THEN ‘May’
-	WHEN 6 THEN ‘June’
-WHEN 7 THEN ‘July’
-WHEN 1 THEN ‘August’
-	WHEN 2 THEN ‘September’
-WHEN 3 THEN ‘October’
-	WHEN 4 THEN ‘November’
-WHEN 5 THEN ‘December’
-END AS Month, COUNT(MONTH) AS ‘Num of Flights’
+	WHEN 1 THEN 'January'
+	WHEN 2 THEN 'February'
+	WHEN 3 THEN 'March'
+	WHEN 4 THEN 'April'
+	WHEN 5 THEN 'May'
+	WHEN 6 THEN 'June'
+	WHEN 7 THEN 'July'
+	WHEN 8 THEN 'August'
+	WHEN 9 THEN 'September'
+	WHEN 10 THEN 'October'
+	WHEN 11 THEN 'November'
+	WHEN 12 THEN 'December'
+    END AS 'Month', COUNT(MONTH) AS 'TotalFlights'
 FROM flights
 GROUP BY Month
-ORDER BY ‘Num of Flights’ DESC;
+ORDER BY 'TotalFlights' DESC;
 
-“Make a table that shows AIRLINE and total number of flights.”
 
-SELECT flights.AIRLINE AS Airline, airlines.AIRLINE AS ID, COUNT(flights.AIRLINE) AS ‘Num of Flights’
+SELECT flights.AIRLINE AS Airline, airlines.AIRLINE AS ID, COUNT(flights.AIRLINE) AS 'TotalFlights'
 FROM flights
 JOIN airlines
 ON 
