48cca04ca34fad73eb42ee301a02cbd8d460014d
Nancy Luna <nlunayb04@gmail.com>
2026-05-28 21:50:31 -0700

    3rd visual

--- streamlit/Months.py
+++ streamlit/Months.py
@@ -7,6 +7,8 @@ import plotly.express as px
 
 month_df = pd.read_csv("Busiest_months.csv")
 days_df = pd.read_csv("Busiest_days.csv")
+airlines_df = pd.read_csv("Busiest_airlines.csv")
+
 def show_months():
     st.title("Flight Frequency per month")
     st.write("What Months have the most frequent flights?")
@@ -38,9 +40,38 @@ def show_months():
         labels={
             "TotalFlights": "Number of Flights"
         },
-        title="Flights by Day of the Week"
+        color = "day of week",
+        color_discrete_map={
+            "Monday": "blue",
+            "Wednesday": "blue",
+            "Thursday": "blue",
+            "Tuesday": "lightblue",
+            "Sunday": "lightblue",
+            "Saturday": "lightblue",
+            "Friday": "lightblue"
+        },
+        title="Flights by Day of the Week"  
     )
 
     fig2.update_layout(xaxis_tickangle=45)
 
     st.plotly_chart(fig2, use_container_width=True)
+
+
+    st.write('Which Airlines have the most flights?')
+
+    fig3 = px.bar(
+        airlines_df,
+        x="Airline",
+        y="TotalFlights",
+        text="TotalFlights",
+        labels={
+            "TotalFlights": "Number of Flights"
+        },
+        color = "ID",
+        title="Busiest Airlines"  
+    )
+
+    fig3.update_layout(xaxis_tickangle=45)
+
+    st.plotly_chart(fig3, use_container_width=True)
