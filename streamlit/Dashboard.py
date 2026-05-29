327ba1439d612f863477ba7e10f9d6883bd7a005
Nancy Luna <nlunayb04@gmail.com>
2026-05-29 11:51:23 -0700

    formatting update

--- streamlit/Dashboard.py
+++ streamlit/Dashboard.py
@@ -6,14 +6,14 @@ from DelayAnalysis import show_delay_analysis
 
 st.title("Flight Delay Dashboard")
 tab1, tab2, tab3 = st.tabs(
-    ["Times of Frequent Flights", "Predictor", "Delay Analysis"]
+    ["Times of Frequent Flights", "Delay Analysis", "Predictor"]
 )
 
 with tab1:
     show_months()
 
 with tab2:
-    show_predictor()
+    show_delay_analysis()
 
 with tab3:
-    show_delay_analysis()
+    show_predictor()
